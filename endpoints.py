347793d738b2198ded5c8d8bc0683455d7713ec9
Matt Rosanio <matt.rosanio@acceptto.com>
2018-12-26 16:59:40 -0500

    Set up heartbeat root endpoints

--- endpoints.py
+++ endpoints.py
@@ -3,6 +3,10 @@ from .wanikanireverse import get_cards_from_database
 
 app = Flask(__name__)
 
+@app.route('/')
+def everythings_file():
+    return 'Everything is wonderful'
+
 @app.route('/get_burned_cards')
 def burned_cards():
     cards = get_cards_from_database()
