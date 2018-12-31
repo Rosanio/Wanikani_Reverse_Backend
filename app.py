e9d2f69dee20d7194387b91278e4778a98fee12e
Matt Rosanio <matt.rosanio@acceptto.com>
2018-12-31 16:09:05 -0500

    Make get_burned_cards api allow CORS

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 try:
     db_url = os.environ['DATABASE_URL']
@@ -11,6 +12,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 try:
     from wanikanireverse import get_cards_from_database
@@ -24,6 +27,7 @@ def everythings_file():
     return 'Everything is wonderful'
 
 @app.route('/get_burned_cards')
+@cross_origin
 def burned_cards():
     cards = get_cards_from_database()
     response = []
