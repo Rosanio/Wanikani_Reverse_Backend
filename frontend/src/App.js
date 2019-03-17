ef3d40c522dd6116b78f65bc4c5a8fe319ffa5c1
Rosanio <matt.rosanio@gmail.com>
2019-03-17 07:35:56 -0700

    Save cards pulled to state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  
-  render() {
-    console.log('Start')
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    };
+  }
+
+  componentDidMount() {
     fetch('http://localhost:8001/get_burned_cards')
-        .then(data => {
-          console.log('Got data')
-          return data.json()
-        })
-        .then(res => {console.log(res)})
-        .catch(error => {console.log(error)})
+    .then(data => {return data.json()})
+    .then(res => {
+      this.setState({
+        cards: res
+      })
+      console.log(this.state)
+    })
+    .catch(error => {console.log(error)})
+  }
+
+  render() {
     return (
       <div className="App">
         
